34293b6136acac6d00f34e56a2c1aa0170f1cbbd
DiegoPalacios <yeyoPRO8@gmail.com>
2024-04-03 20:02:03 -0500

    ejercicio1

---
+++ Taller 1 Programacion/variable.js
@@ -0,0 +1,20 @@
+const a = 4;
+const b = 7;
+const c = 3;
+const d = 2;
+const y = 3.5;
+const x = 2.0;
+const z = 5.0;
+const w = 1.5;
+
+const resultado1 = a * b / 2 + 1;
+const resultado2 = c / Math.pow(y, 2);
+const resultado3 = (z / x) + b * w * (c - b) / a;
+
+console.log ("Resultado 1:", resultado1)
+console.log ("Resultado 2:", resultado2)
+console.log ("Resultado 3:", resultado3)
+
+document.write("<p>Resultado 1: " + resultado1 + "</p>");
+document.write("<p>Resultado 2: " + resultado2 + "</p>");
+document.write("<p>Resultado 3: " + resultado3 + "</p>");
